9c6570f28a8dceaea7bc043f49eb7e7a7546c827
策君公子开明 <gouxuming@gmail.com>
2023-07-08 13:42:49 +0800

    【手写mybatis框架】入门

--- src/test/java/cn/ming/mybatis/mapper/UserMapper.java
+++ src/test/java/cn/ming/mybatis/mapper/UserMapper.java
@@ -4,7 +4,9 @@ package cn.ming.mybatis.mapper;
  * @Author: xuming
  * @Date: 2023-07-08 13:23
  * @Version: 1.0
- * @Description: TODO
+ * @Description: User表Mapper接口
  **/
 public interface UserMapper {
+
+
 }
