ff9909d8665877a246b3cc2cc47b6019a7376eee
策君公子开明 <gouxuming@gmail.com>
2023-07-15 14:19:25 +0800

    【手写mybatis框架】数据源的创建和使用

--- src/main/java/cn/ming/mybatis/datasource/DataSourceFactory.java
+++ src/main/java/cn/ming/mybatis/datasource/DataSourceFactory.java
@@ -1,10 +1,25 @@
 package cn.ming.mybatis.datasource;
 
+import javax.sql.DataSource;
+import java.util.Properties;
+
 /**
  * @Author: xuming
  * @Date: 2023-07-15 13:37
  * @Version: 1.0
- * @Description: TODO
+ * @Description: 数据源工厂
  **/
 public interface DataSourceFactory {
+
+    String DRIVER = "driver";
+
+    String URL = "url";
+
+    String USERNAME = "username";
+
+    String PASSWORD = "password";
+
+    void setProperties(Properties props);
+
+    DataSource getDataSource();
 }
--- src/main/java/cn/ming/mybatis/datasource/druid/DruidDataSourceFactory.java
+++ src/main/java/cn/ming/mybatis/datasource/druid/DruidDataSourceFactory.java
@@ -1,10 +1,33 @@
 package cn.ming.mybatis.datasource.druid;
 
+import cn.ming.mybatis.datasource.DataSourceFactory;
+import com.alibaba.druid.pool.DruidDataSource;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
 /**
  * @Author: xuming
  * @Date: 2023-07-15 13:36
  * @Version: 1.0
- * @Description: TODO
+ * @Description: Druid数据源工厂
  **/
-public class DruidDataSourceFactory {
+public class DruidDataSourceFactory implements DataSourceFactory {
+
+    private Properties props;
+
+    @Override
+    public void setProperties(Properties props) {
+        this.props = props;
+    }
+
+    @Override
+    public DataSource getDataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setDriverClassName(props.getProperty(DRIVER));
+        druidDataSource.setUrl(props.getProperty(URL));
+        druidDataSource.setUsername(props.getProperty(USERNAME));
+        druidDataSource.setPassword(props.getProperty(PASSWORD));
+        return druidDataSource;
+    }
 }
