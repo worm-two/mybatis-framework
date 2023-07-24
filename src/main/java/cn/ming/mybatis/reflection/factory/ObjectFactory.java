96b3ca0be8214023d808f7074cef7a1156342eaf
策君公子开明 <gouxuming@gmail.com>
2023-07-24 21:38:54 +0800

    【手写mybatis框架】结果集处理器

--- src/main/java/cn/ming/mybatis/reflection/factory/ObjectFactory.java
+++ src/main/java/cn/ming/mybatis/reflection/factory/ObjectFactory.java
@@ -11,15 +11,43 @@ import java.util.Properties;
  **/
 public interface ObjectFactory {
 
-    // 设置属性
+    /**
+     * Sets configuration properties.
+     * 设置属性
+     *
+     * @param properties configuration properties
+     */
     void setProperties(Properties properties);
 
-    // 生产对象
+    /**
+     * Creates a new object with default constructor.
+     * 生产对象
+     *
+     * @param type Object type
+     * @return <T>
+     */
     <T> T create(Class<T> type);
 
-    // 生产对象，使用指定的构造函数和构造函数参数
+    /**
+     * Creates a new object with the specified constructor and params.
+     * 生产对象，使用指定的构造函数和构造函数参数
+     *
+     * @param type                Object type
+     * @param constructorArgTypes Constructor argument types
+     * @param constructorArgs     Constructor argument values
+     * @return <T>
+     */
     <T> T create(Class<T> type, List<Class<?>> constructorArgTypes, List<Object> constructorArgs);
 
-    // 返回这个对象是否是集合
+    /**
+     * Returns true if this object can have a set of other objects.
+     * It's main purpose is to support non-java.util.Collection objects like Scala collections.
+     * 返回这个对象是否是集合，为了支持 Scala collections
+     *
+     * @param type Object type
+     * @return whether it is a collection or not
+     * @since 3.1.0
+     */
     <T> boolean isCollection(Class<T> type);
+
 }
